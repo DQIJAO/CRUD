2510f059baca7882e6d4616772b62b0c8fbf847e
JIRAWAT JARUTLUECHAI <34392290+DQIJAO@users.noreply.github.com>
2018-09-24 11:53:43 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/CRUD/pkg/app"
 	"github.com/CRUD/pkg/model"
 )
 
 const (
-	port     = ":8080"
-	mongoURL = "mongodb://127.0.0.1:27017"
+	//port = ":8080"
+	//mongoURL = "mongodb://127.0.0.1:27017"
+	mongoURL = "mongodb://gonews:Paopao001@ds111913.mlab.com:11913/gonews"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	mux := http.NewServeMux()
-
 	app.Mount(mux)
 	err := model.Init(mongoURL)
 	if err != nil {
 		log.Fatalf("can not init model; %v ", err)
 	}
-	http.ListenAndServe(port, mux)
+	http.ListenAndServe(":"+port, mux)
 }
